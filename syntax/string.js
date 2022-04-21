75e0466f27cd18fe6ed49cca871f49353ea5d22d
Woody <nz.wgkim@gmail.com>
2022-04-21 18:54:25 +1200

    Node.js - 10.URL을 통해서 입력된 값 사용하기

---
+++ syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'4');
+'Loren  
---
+++ syntax/variable.js
@@ -0,0 +1,8 @@
+a='Woody';
+b='Lorem, ipsum '+a+' \n\ndolor sit amet consectetur '+a+'adipisicing elit. '+a+'Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?';
+console.log(b);
+
+c=`Lorem, ipsum ${a} 
+
+dolor sit amet consectetur ${a}adipisicing elit. ${a}Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?`;
+console.log(c);;
