6d8306175940b125b6a13b01fe749c30e855703f
Woody <nz.wgkim@gmail.com>
2022-06-01 06:03:32 +1200

    상속지정하는 방법 __proto__

--- syntax/inherit.js
+++ syntax/inherit.js
@@ -1,5 +1,3 @@
-const { read } = require("fs");
-
 const car = {
     wheels: 4,
     drive(){
@@ -22,3 +20,28 @@ benz.__proto__ = car;
 audi.__proto__ = car;
 console.log(bmw, bmw.wheels);
 bmw.drive();
+for( x in bmw){
+    if(bmw.hasOwnProperty(x)){
+        console.log('o', x);
+    }
+    else{
+        console.log('x', x);
+    }
+}
+
+
+const Bmw = function(color){
+    this.color = color;
+}
+const x5 = new Bmw();
+const z4 = new Bmw();
+
+x5.__proto__ = car;
+for( p in x5){
+    if(x5.hasOwnProperty(p)){
+        console.log('o', p);
+    }
+    else{
+        console.log('x',p);
+    }
+}
