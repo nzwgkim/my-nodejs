148fcdf6d2bedf2d037624862b8d8245d8bc5670
Woody <nz.wgkim@gmail.com>
2022-06-03 05:24:21 +1200

    multiple then to see the procedure of then().catch().finally()

--- syntax/promise.js
+++ syntax/promise.js
@@ -20,7 +20,7 @@ const pr = new Promise((resolve,reject)=>{
 // 방2 prefered!
 pr.then(
     function(result){
-        console.log(result);
+        console.log(result,'=1');
     }
 ).catch(
     function(err){
@@ -28,6 +28,14 @@ pr.then(
     }
 ).finally(  // 항상 실행됨. 긴 시간의 loading 화면을 없앨때 유용하다.
     function(){
-        console.log('Finish!');
+        console.log('=Final');
     }    
 );
+
+pr.then((result)=>{
+    console.log(result, '-2');
+}).catch((err)=>{
+    console.log(err);
+}).finally(()=>{
+    console.log('-Final');
+});
